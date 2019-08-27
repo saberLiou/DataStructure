7d25eb96eac271ac6ca99407a7027ac2fbf6cd4d
Guo-Xun Liu <saberliou@gmail.com>
2019-08-27 15:19:28 +0800

    modify gcc_exec.sh.

--- gcc_exec.sh
+++ gcc_exec.sh
@@ -2,5 +2,6 @@
 
 # Set compiling and executing by gcc in one alias function.
 function gcc_exec() {
+	if [ ! -d exec ]; then mkdir exec; fi	# mkdir exec if not exist.
 	if gcc -o exec/${1} ${1}.c; then ./exec/${1}; fi
 }
